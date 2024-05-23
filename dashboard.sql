ff0727afc5f48f0658a216c943710bc2f312acbf
joao-eduardo17 <125677846+joao-eduardo17@users.noreply.github.com>
2024-05-23 15:43:13 -0300

    Update dashboard.sql

--- dashboard.sql
+++ dashboard.sql
@@ -1,13 +1,13 @@
 CREATE DATABASE DASHBOARD;
 USE DASHBOARD;
--- DROP DATABASE DASHBOARD;
+# DROP DATABASE DASHBOARD;
 CREATE TABLE usuarios (
 	usuario_id INT PRIMARY KEY AUTO_INCREMENT, 	
 	usuario_nome VARCHAR(50),
 	usuario_cpf VARCHAR(15),
 	usuario_email VARCHAR(70),
 	usuario_senha VARCHAR(35),
-	administrador BOOL
+	administrador VARCHAR(1) DEFAULT 0
 );
 
 CREATE TABLE clientes (
@@ -17,10 +17,15 @@ CREATE TABLE clientes (
 	cliente_segmento VARCHAR(25)
 );
 
+CREATE TABLE produtos (
+	produto_id INT PRIMARY KEY AUTO_INCREMENT,
+    produto_nome VARCHAR(60),
+    produto_valor FLOAT
+);
+
 CREATE TABLE vendas (
     venda_id INT PRIMARY KEY AUTO_INCREMENT,
     venda_data DATE,
-    venda_produto VARCHAR(35),
     cliente_id INT,
     FOREIGN KEY (cliente_id) REFERENCES clientes (cliente_id),
     produto_id INT,
@@ -29,27 +34,20 @@ CREATE TABLE vendas (
     FOREIGN KEY (usuario_id) REFERENCES usuarios (usuario_id)
 );
 
-CREATE TABLE produtos (
-	produto_id INT PRIMARY KEY AUTO_INCREMENT,
-    produto_nome VARCHAR(60),
-    produto_valor FLOAT
-);
-
-
 -- Inserir valores na tabela usuario
-INSERT INTO usuario (usuario_nome, usuario_cpf, usuario_email, usuario_senha, administrador)
+INSERT INTO usuarios (usuario_nome, usuario_cpf, usuario_email, usuario_senha, administrador)
 VALUES ('João Silva', '123.456.789-00', 'joao@email.com', 'senha123', 1);
 
 -- Inserir valores na tabela cliente
-INSERT INTO cliente (cliente_nome, cliente_cpfcnpj, cliente_segmento)
+INSERT INTO clientes (cliente_nome, cliente_cpfcnpj, cliente_segmento)
 VALUES ('Empresa XYZ', '987.654.321-00', 'Tecnologia');
 
 -- Inserir valores na tabela vendas
-INSERT INTO vendas (venda_data, venda_produto, cliente_id, venda_valor, usuario_id)
-VALUES ('2024-05-13', 'Produto A', 1, 100.00, 1);
+-- INSERT INTO vendas (venda_data, venda_produto, cliente_id, venda_valor, usuario_id)
+-- VALUES ('2024-05-13', 'Produto A', 1, 100.00, 1);
 
 -- Mostrar valores da tabela usuario
-SELECT * FROM usuario;
+SELECT * FROM usuarios;
 
 -- Mostrar valores da tabela cliente
 -- SELECT * FROM cliente;
